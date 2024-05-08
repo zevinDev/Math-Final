4403366a8b4afa2d485a83680288d1cb866267f5
Zevin Burzynski <86094612+zevinDev@users.noreply.github.com>
2024-05-08 08:55:41 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,88 +1,107 @@
-const cors = require('cors');
-const io = require('socket.io')(process.env.PORT || 3000, {
+const io = require("socket.io")(process.env.PORT || 3000, {
   cors: {
-    origin: "https://d3fd94a85cc9.ngrok.app",
+    origin:
+      "https://run-math-final-0990160b5a86.herokuapp.com:3000/",
     methods: ["GET", "POST"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 let clientRooms = {};
 let state = {};
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "../../frontend/index.html"));
+});
+app.listen(8080, () => {
+  console.log("Server successfully running on port 8080");
+});
 
-io.on('connection', (client) => {
-  client.on('newGame', handleNewGame);
-  client.on('joinGame', handleJoinGame);
-  client.on('startQuiz', () => startQuiz(client));
+io.on("connection", (client) => {
+  client.on("newGame", handleNewGame);
+  client.on("joinGame", handleJoinGame);
+  client.on("startQuiz", () => startQuiz(client));
 
   function handleNewGame(nickName) {
     let roomName = makeid(5);
     clientRooms[client.id] = roomName;
     state[roomName] = {
-      players: [
-        { nickName: nickName, points: 0, id: client.id},
-      ],
+      players: [{ nickName: nickName, points: 0, id: client.id }],
     };
     client.join(roomName); // client joins the room
-    client.emit('gameCode', roomName);
-    io.to(roomName).emit('playerJoined', nickName); // emit to all clients in the room
+    client.emit("gameCode", roomName);
+    io.to(roomName).emit("playerJoined", nickName); // emit to all clients in the room
   }
 
   function handleJoinGame(roomName, nickName) {
     let numClients = state[roomName].players.length;
 
     if (numClients === 0) {
-      client.emit('unknownCode');
+      client.emit("unknownCode");
       return;
     } else if (numClients >= 4) {
-      client.emit('tooManyPlayers');
+      client.emit("tooManyPlayers");
       return;
     }
 
     clientRooms[client.id] = roomName;
     client.join(roomName); // client joins the room
-    state[roomName].players.push({ nickName: nickName, points: 0, id: client.id});
-    client.emit('gameCode', roomName);
-    io.to(roomName).emit('playerJoined', state[roomName].players.map(player => player.nickName)); // emit to all clients in the room
+    state[roomName].players.push({
+      nickName: nickName,
+      points: 0,
+      id: client.id,
+    });
+    client.emit("gameCode", roomName);
+    io.to(roomName).emit(
+      "playerJoined",
+      state[roomName].players.map((player) => player.nickName),
+    ); // emit to all clients in the room
   }
 
-  function startQuiz(client){
+  function startQuiz(client) {
     let roomName = clientRooms[client.id];
-    let questions = require('./questions.json');
+    let questions = require("./questions.json");
     let timerId;
     let currentQuestion = getRandomQuestion(questions);
-    
+
     function sendQuestion() {
       return new Promise((resolve) => {
-        if(timerId){
+        if (timerId) {
           clearTimeout(timerId);
         }
-  
+
         currentQuestion = getRandomQuestion(questions);
-        io.to(roomName).emit('question', { 
-          text: currentQuestion.text, 
-          options: currentQuestion.options 
+        io.to(roomName).emit("question", {
+          text: currentQuestion.text,
+          options: currentQuestion.options,
         });
-  
+
         timerId = setTimeout(sendQuestion, 140 * 1000); // 10 seconds
         resolve();
       });
     }
-  
-    client.on('answer', (answer) => {
+
+    client.on("answer", (answer) => {
       // Check if the answer is correct
       if (answer === currentQuestion.answer) {
         sendQuestion().then(() => {
           // Call the function to move the car
-          io.to(roomName).emit('carMoved', state[roomName].players.find(player => player.id === client.id).nickName);
+          io.to(roomName).emit(
+            "carMoved",
+            state[roomName].players.find((player) => player.id === client.id)
+              .nickName,
+          );
         });
       } else {
-        client.emit('incorrectAnswer');
+        client.emit("incorrectAnswer");
       }
     });
-  
+
     sendQuestion();
   }
-  
+
   function getRandomQuestion(questions) {
     let randomIndex = Math.floor(Math.random() * questions.length);
     return questions[randomIndex];
@@ -90,8 +109,9 @@ io.on('connection', (client) => {
 });
 
 function makeid(length) {
-  let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
